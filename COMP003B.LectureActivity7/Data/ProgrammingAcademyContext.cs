b33ba154d441dd578b0a3adeeca75993ac8681d2
kylejames1998 <kylesacct409@outlook.com>
2024-05-06 18:35:38 -0700

    added context

---
+++ COMP003B.LectureActivity7/Data/ProgrammingAcademyContext.cs
@@ -0,0 +1,17 @@
+﻿using COMP003B.LectureActivity7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.LectureActivity7.Data
+{
+    public class ProgrammingAcademyContext : DbContext
+    {
+        public ProgrammingAcademyContext(DbContextOptions<ProgrammingAcademyContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
